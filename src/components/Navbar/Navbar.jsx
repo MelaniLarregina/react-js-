84afb7f374d76c4f3a564160b1a9cf647beb20ea
Melani Larregina <larreginamelani22@gmail.com>
2024-06-23 23:39:36 -0300

    sumo carrito

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,16 @@
 import './Navbar.css'
+import { RiShoppingCartLine } from "react-icons/ri";
 
 const Navbar = () => {
     return <div className='bar'>
+      <h2>logo</h2>
     <ul>
       <li>Inicio</li>
       <li>Productos</li>
       <li>Informacion</li>
     </ul>
+    <RiShoppingCartLine color='black' size={40}/>
+    <span className='span'>0</span>
     </div>
    
     
