c7e12a88492f8a4174b47e7e2ca2fbe013dbf206
Melani Larregina <larreginamelani22@gmail.com>
2024-06-24 18:47:47 -0300

    agregue foto en el logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import CartWidget from '../../../CardWidget/CardWidget';
 import './Navbar.css'
 
+
 const Navbar = () => {
     return <div className='bar'>
-      <h2>logo</h2>
+      <img className='logo'
+       src="https://res.cloudinary.com/dvjzcl7ux/image/upload/c_thumb,w_200,g_face/v1719264897/beauty-salon-logo-makeup-tools-cosmetic-brushes-lipstick-blush_528132-433_oe49cx.avif" alt="" />
     <ul>
       <li>Inicio</li>
       <li>Productos</li>
